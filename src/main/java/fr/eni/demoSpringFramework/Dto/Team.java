d0cf68993f3a80dddc5c9c3cf7741c7d565659fc
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2025-10-14 12:11:59 +0200

    feat(teams): add/delete team

--- src/main/java/fr/eni/demoSpringFramework/Dto/Team.java
+++ src/main/java/fr/eni/demoSpringFramework/Dto/Team.java
@@ -1,9 +1,16 @@
 package fr.eni.demoSpringFramework.Dto;
 
+import java.util.ArrayList;
+
 public class Team {
 
     private int id;
     private String name;
+    private ArrayList<Player> players;
+
+    {
+        players = new ArrayList<>();
+    }
 
     public Team(String name) {
         this.name = name;
@@ -23,5 +30,21 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
     }
 }
