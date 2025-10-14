db9c009b01737de38bbd18ba8c6ba6d395443e07
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2025-10-14 10:18:41 +0200

    init

---
+++ src/main/java/fr/eni/demoSpringFramework/DemoSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.demoSpringFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringFrameworkApplication.class, args);
+	}
+
+}
