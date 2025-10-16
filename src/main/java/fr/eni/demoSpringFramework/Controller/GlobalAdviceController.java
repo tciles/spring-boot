941bd6b5d0a769e300c497932b4108c7cd518eb2
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2025-10-16 15:39:08 +0200

    Init sql

--- src/main/java/fr/eni/demoSpringFramework/Controller/GlobalAdviceController.java
+++ src/main/java/fr/eni/demoSpringFramework/Controller/GlobalAdviceController.java
@@ -3,6 +3,7 @@ package fr.eni.demoSpringFramework.Controller;
 import fr.eni.demoSpringFramework.Response.Payload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 public class GlobalAdviceController {
 
     /**
-     * Handle Not Resource Found
+     * Handle Not Resource Found Exception
      *
      * @return The Response.
      */
@@ -22,4 +23,15 @@ public class GlobalAdviceController {
                 .body(Payload.create(null, "Page Not Found", HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * Handle Http Request Method Not Supported Exception
+     *
+     * @return The Response.
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> handleHttpRequestMethodNotSupportedException() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Payload.create(null, "Bad Request", HttpStatus.BAD_REQUEST));
+    }
 }
